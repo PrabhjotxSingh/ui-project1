fb3451af766561303e1f19ce024330c7d719f870
Prabhjot Singh <97115364+PrabhjotxSingh@users.noreply.github.com>
2024-09-12 21:48:54 -0400

    initial commit

---
+++ cinepath/src/App.svelte
@@ -0,0 +1,10 @@
+<script>
+    import WelcomeBanner from './lib/WelcomeBanner.svelte';
+</script>
+
+<main>
+    <WelcomeBanner />
+</main>
+
+<style>
+</style>
---
+++ cinepath/src/lib/WelcomeBanner.svelte
@@ -0,0 +1,10 @@
+<style>
+    .banner{
+        width: 100%;
+        height: 50vh;
+        background-color: black;
+    }
+    </style>
+
+    <div class="banner">
+    </div>
